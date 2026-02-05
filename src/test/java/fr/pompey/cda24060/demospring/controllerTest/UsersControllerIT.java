0fb259554d52080c166007792389b9ff81de6829
marjorie40 <marjoriemazeau2@gmail.com>
2026-02-05 13:44:59 +0100

    maj pom mockito

--- src/test/java/fr/pompey/cda24060/demospring/controllerTest/UsersControllerIT.java
+++ src/test/java/fr/pompey/cda24060/demospring/controllerTest/UsersControllerIT.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class UsersControllerTIntegration {
+public class UsersControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
