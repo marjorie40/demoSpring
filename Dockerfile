6299721cd7debac983c3a184ffb391c80b05094b
marjorie40 <marjoriemazeau2@gmail.com>
2026-02-11 14:11:08 +0100

    mise a jour workflow cicd

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Image runtime Java 21 jre OK mais mieux si on utilise :21-jdk
+FROM eclipse-temurin:21-jre
+
+# Repertoire de travail
+WORKDIR /app
+
+# Copier le jar genere
+COPY target/demoSpring-0.0.1-SNAPSHOT.jar api.jar
+
+# Exposer le port de l'API
+EXPOSE 9000
+
+# Demarrer l'application Spring Boot
+ENTRYPOINT ["java", "-jar", "api.jar"]
--- src/test/java/fr/pompey/cda24060/demospring/controllerTest/UsersControllerIT.java
+++ src/test/java/fr/pompey/cda24060/demospring/controllerTest/UsersControllerIT.java
@@ -1,5 +1,9 @@
 package fr.pompey.cda24060.demospring.controllerTest;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,6 +28,8 @@ public class UsersControllerIT {
      * @throws Exception
      */
     @Test
+    @Description ("Test de récupération de la listes des utilisateurs (users)")
+    @Severity(SeverityLevel.CRITICAL)
     public void getUsersTest() throws Exception {
         performGetAllUsersRequest();
     }
@@ -32,6 +38,8 @@ public class UsersControllerIT {
      * Methode de controle du prenom du premier users "Julien"
      * @throws Exception
      */
+
+    @Step("Effectuer une requete GET pour recuperer la liste des utilisateurs")
     private void performGetAllUsersRequest() throws Exception {
         mockMvc.perform(get("/all-users"))
                 .andExpect(status().isOk())
